e137e6d8caf496059d9bc4a291fb96f0938a4ded
pablodecm <pablodecm@gmail.com>
2015-12-07 14:43:21 +0100

    fix DELETE_OUTPUT_DIRECTORY and add Google Analytics

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,7 +38,8 @@ CC_LICENSE='CC-BY-NC-SA'
 STATIC_PATHS = ['extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
-DELETE_OUTPUT_DIRECTORY = False
+GOOGLE_ANALYTICS='UA-71094563-1'
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
