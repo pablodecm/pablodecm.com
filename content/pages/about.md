5dd8d1a7bb786ce0445a2c81a9dc9b5c7def2df4
pablodecm <pablodecm@gmail.com>
2015-12-07 14:24:45 +0100

    Empty Pelican blog, using pelican-bootstrap3 theme
    No contents, only what was generated with pelican-quickstart
    and some dummy about page. CNAME for pablodecm.com has been
    added add included in STATIC PATHS.

---
+++ content/pages/about.md
@@ -0,0 +1,3 @@
+Title: About
+
+My name is Pablo de Castro.
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Pablo de Castro'
+SITENAME = u'pablodecm.com'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ((u'AMVA4NewPhysics', 'https://amva4newphysics.wordpress.com/'),
+         (u'Tommaso Dorigo', 'http://www.science20.com/quantum_diaries_survivor'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/pablodecm'),
+          ('github', 'http://github.com/pablodecm'),)
+
+
+DEFAULT_PAGINATION = 10
+
+THEME=u'./pelican-bootstrap3'
+BOOTSTRAP_THEME=u'simplex'
+
+CC_LICENSE='CC-BY-NC-SA'
+
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+DELETE_OUTPUT_DIRECTORY = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
